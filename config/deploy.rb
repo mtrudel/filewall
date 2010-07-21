839c0d600ee67a2f189a79d21b1c008904284d28
Mat Trudel <mat@geeky.net>
2010-07-21 17:57:17 -0400

    Cleanup of base deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,30 @@
+#
+# Server setup -- this will vary depending on your firewall's particulars
+#
+role :firewall, "YOUR FIREWALL HOSTNAME OR IP HERE"
+set :user, "YOUR REMOTE USER HERE"
 set :deploy_to, "/var/filewall"
 
-role :firewall, "YOUR IP HERE"
-
-set :user, "ubuntu" # Change this on debian machines
+# this is necessary for Debian and Ubuntu machines to properly use sudo
 default_run_options[:pty] = true
+
+
+#
+# Repository setup -- this will vary depending on your scm setup
+#
+set :scm, :git
+set :repository, "YOUR REPO HERE"
+set :branch, "YOUR BRANCH HERE"
+
+# Setting the following makes outbound git connections on the server work
 ssh_options[:forward_agent] = true
 
-set :scm, :git
-set :repository, "YOUR REPO PATH HERE"
-set :branch, "master" # You'll probably want to change this
 
-set :rule_timeout, 60 # How long wait before reverting rules
+#
+# Filewall setup
+#
+set :rule_timeout, 60   # How long should shorewall wait before reverting rules
 
+
+# Load the actual recipes
 load 'config/filewall'
