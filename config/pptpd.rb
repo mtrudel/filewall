4cb081df3fd0b9700ab1fff6994caa8fc562dcb1
Mat Trudel <mat@geeky.net>
2010-07-21 16:38:58 -0400

    Add pap and chap secrets

--- config/pptpd.rb
+++ config/pptpd.rb
@@ -3,6 +3,8 @@ namespace :pptpd do
   task :symlink do
     run "[ ! -f /etc/pptpd.conf ] || (#{sudo} mv /etc/pptpd.conf /etc/pptpd.conf.orig && #{sudo} ln -s #{deploy_to}/current/pptpd/pptpd.conf /etc/pptpd.conf)"
     run "[ ! -f /etc/ppp/pptpd-options ] || (#{sudo} mv /etc/ppp/pptpd-options /etc/ppp/pptpd-options.orig && #{sudo} ln -s #{deploy_to}/current/pptpd/pptpd-options /etc/ppp/pptpd-options)"
+    run "[ ! -f /etc/ppp/chap-secrets ] || (#{sudo} mv /etc/ppp/chap-secrets /etc/ppp/chap-secrets.orig && #{sudo} ln -s #{deploy_to}/current/pptpd/chap-secrets /etc/ppp/chap-secrets)"
+    run "[ ! -f /etc/ppp/pap-secrets ] || (#{sudo} mv /etc/ppp/pap-secrets /etc/ppp/pap-secrets.orig && #{sudo} ln -s #{deploy_to}/current/pptpd/pap-secrets /etc/ppp/pap-secrets)"    
   end  
     
   desc "Apply the curernt pptpd config"
